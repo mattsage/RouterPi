220f93694d4b26dddb1e1bd2a130a249296cc5a7
Matthew Sage <mattsage@users.noreply.github.com>
2017-03-24 09:37:50 +0000

    Update TorrentVPN.sh

--- TorrentVPN.sh
+++ TorrentVPN.sh
@@ -4,13 +4,13 @@ echo $OldIP
 echo "****************************************"
 cd /etc/openvpn/
 sudo service transmission-daemon reload 
-sudo openvpn --config /etc/openvpn/UK\ Southampton.ovpn --auth-user-pass /etc/openvpn/login.txt &
+sudo openvpn --config /etc/openvpn/UK\ Southampton.ovpn --auth-user-pass /et$
 sleep 5
 yes "" | echo "VPN Connected"
 NewIP=`wget http://ipinfo.io/ip -qO -`
 echo "****************************************"
 echo $NewIP
 echo "****************************************"
+/usr/bin/Pushbullet.sh "Old IP: $OldIP New IP: $NewIP"
 gpio mode 14 out
 gpio write 14 1
-sudo python /home/pi/Shutdown.py &
