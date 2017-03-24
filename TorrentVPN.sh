45fcad061ccb0aad4d82cd4cbb0100e3c3425663
Matt Sage <mattsage@rocketmail.com>
2017-03-24 14:22:40 +0000

    Blink LED on Failure to connect to VPN

--- TorrentVPN.sh
+++ TorrentVPN.sh
@@ -12,21 +12,24 @@ echo "****************************************"
 echo $NewIP
 echo "****************************************"
 
+gpio mode 14 out
+
 if [ $OldIP = $NewIP ]
 then
 	echo "VPN Failed to connect"
 	/usr/bin/Pushbullet.sh "VPN Failed to connect"
-  blink = 0
-	while [ $blink -le 20]
-  do
-    gpio mode 14 out
-	  gpio write 14 1
-    sleep 1
-    $blink = $(( $blink+1 ))
-  done
+	blink=0
+	while [ $blink -le 20 ]
+	do
+		#gpio mode 14 out
+		gpio write 14 1
+		sleep 1
+		gpio write 14 0
+		(( blink++ ))
+	done
 else
 	/usr/bin/Pushbullet.sh "Old IP: $OldIP New IP: $NewIP"
-	gpio mode 14 out
+	#gpio mode 14 out
 	gpio write 14 1
 	sudo python /home/pi/RouterPi/Shutdown.py
 fi
