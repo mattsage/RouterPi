2f5c87c024a3457f616b8b18fbea7e3a7d9bd61b
Matthew Sage <mattsage@users.noreply.github.com>
2017-03-24 13:50:04 +0000

    Update TorrentVPN.sh
    added While Loop

--- TorrentVPN.sh
+++ TorrentVPN.sh
@@ -4,14 +4,30 @@ echo $OldIP
 echo "****************************************"
 cd /etc/openvpn/
 sudo service transmission-daemon reload 
-sudo openvpn --config /etc/openvpn/UK\ Southampton.ovpn --auth-user-pass /et$
+sudo openvpn --config /etc/openvpn/UK\ Southampton.ovpn --auth-user-pass /etc/openvpn/login.txt &
 sleep 5
 yes "" | echo "VPN Connected"
 NewIP=`wget http://ipinfo.io/ip -qO -`
 echo "****************************************"
 echo $NewIP
 echo "****************************************"
-/usr/bin/Pushbullet.sh "Old IP: $OldIP New IP: $NewIP"
-gpio mode 14 out
-gpio write 14 1
-sudo python /home/pi/RouterPi/Shutdown.py
+
+if [ $OldIP = $NewIP ]
+then
+	echo "VPN Failed to connect"
+	/usr/bin/Pushbullet.sh "VPN Failed to connect"
+  blink = 0
+	while [ $blink -le 20]
+  do
+    gpio mode 14 out
+	  gpio write 14 1
+    sleep 1
+    $blink = $(( $blink+1 ))
+  done
+  break
+else
+	/usr/bin/Pushbullet.sh "Old IP: $OldIP New IP: $NewIP"
+	gpio mode 14 out
+	gpio write 14 1
+	sudo python /home/pi/RouterPi/Shutdown.py
+fi
