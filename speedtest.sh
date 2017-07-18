89f6a2efb2f866b0b466ed6279ec4cd4ee9e0b7b
Matthew Sage <mattsage@users.noreply.github.com>
2017-07-18 14:55:21 +0100

    Update speedtest.sh

--- speedtest.sh
+++ speedtest.sh
@@ -1,4 +1,10 @@
 #!/bin/bash
 
+########################################################################################
+# Author: Matthew Sage                                            									   #
+# Date: 17/07/2017		                                															   #
+# Description: Does internet Speed Test                                                #
+########################################################################################
+
 date >> /home/pi/speedtest.log
 /usr/local/bin/speedtest --simple >> /home/pi/speedtest.log
