cec81fa66f09be54ae2ca47b55b19d92c2711014
Matthew Sage <mattsage@users.noreply.github.com>
2017-07-18 14:14:45 +0100

    Update Home2Work.py

--- Traffic/Home2Work.py
+++ Traffic/Home2Work.py
@@ -1,6 +1,15 @@
+#!/usr/bin/env python
 import ssl
 import simplejson, urllib
 import gpiozero
+from pushbullet import Pushbullet
+
+#Pushbullet setup
+api_key = open('/home/pi/Pushbulletkey.config', 'r').read() #read Pushbullet Key from /home/pi/Pushbulletkey.config file
+api_key = api_key.replace("\n", "") #Remove Whitespace
+#print api_key
+pb = Pushbullet(api_key) 
+
 context = ssl._create_unverified_context()
 origin = open('/home/pi/RouterPi/Traffic/home.config', 'r').read()
 destination = open('/home/pi/RouterPi/Traffic/work.config', 'r').read()
@@ -16,10 +25,18 @@ driving_time = result['rows'][0]['elements'][0]['duration_in_traffic']['text']
 #    myfile.write("Time: %s" % driving_time)
 dt2 = int(filter(str.isdigit, driving_time))
 print dt2
+dt3 = "ETA to home is %d Minutes" % (dt2)
 
 if dt2 <= 35:
 	print "good"
+	TravelString = "The Traffic is Good, ETA: %d Minutes" % (dt2)
+	push = pb.push_note(dt3, TravelString)
 elif dt2 >= 36 or dt2 <= 40:
 	print "moderate"
+	TravelString = "The Traffic is Moderate, ETA: %d Minutes" % (dt2)
+	push = pb.push_note(dt3, TravelString)
 else:
 	print "heavy"
+	TravelString = "WTF Traffic is a nightmare, ETA: %d Minutes" % (dt2)
+	push = pb.push_note(dt3, TravelString)
+
