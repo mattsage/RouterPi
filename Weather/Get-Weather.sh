7c1fe800646f1d9e0e9621005a743991182f2120
Matthew Sage <mattsage@users.noreply.github.com>
2017-07-21 13:58:47 +0100

    Create Get-Weather.sh

---
+++ Weather/Get-Weather.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+##################################
+#Name: GetWeather.sh
+#Author: Matthew Sage
+#Latest Update: 21/07/17
+#Fetches Daily Weather from WeatherUnderground
+##################################
+
+#Script ot be ran at 6am daily via cron job
+#crontab -e
+# 00 06 * * * /home/pi/RouterPi/Weather/Get-Weather.sh
+
+apikey=`cat /home/pi/APIConfigs/WUapikey.config` #Get WeatherUnderground API key from apikey.config
+location=`cat /home/pi/APIConfigs/WUlocation.config`
+
+pywu fetch $apikey $location #Outputs File to /tmp/pywu.cache.json
