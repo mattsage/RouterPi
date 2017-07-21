9a3a9fd600cf3ba2fb53c44243cc0101bc57024c
Matthew Sage <mattsage@users.noreply.github.com>
2017-07-21 13:44:23 +0100

    Update Pushbullet.sh

--- Pushbullet.sh
+++ Pushbullet.sh
@@ -10,7 +10,7 @@
 #chmod 755 /usr/bin/Pushbullet.sh
 #usage: Pushbullet.sh "Send this"
 
-API=`cat /home/pi/pushbullet.config`
+API=`cat /home/pi/APIConfigs/pushbullet.config`
 MSG="$1"
 
 curl -u $API: https://api.pushbullet.com/v2/pushes -d type=note -d title="Alert" -d body="$MSG"
