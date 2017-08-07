d424bc71931c3d5b2390b9c96e51739dfc8fcf8c
Matthew Sage <mattsage@users.noreply.github.com>
2017-08-07 15:55:15 +0100

    Update FileChecker.sh

--- FileChecker.sh
+++ FileChecker.sh
@@ -5,14 +5,16 @@
 # Date: 17/07/2017																																			          #                                                                    			   #
 # Description: Checks file location for finished downloads, once complete LED on GPIO ??? is lit  #
 ###################################################################################################
-#!/bin/bash
+#foldercount=`ls -1 | wc -l`
 foldercount=`ls /home/pi/Downloads/ -1 | wc -l`
 
 while [ $foldercount != 0 ]
 do
 	echo "$foldercount files left"
-	sleep 5m #Sleep for 5 Mins
+	sleep 15
+#	foldercount=`ls -1 | wc -l`
 	foldercount=`ls /home/pi/Downloads/ -1 | wc -l`
+	find /home/pi/Downloads -name "*.torrent" -type f -delete
 done
 
 echo "No Files left"
