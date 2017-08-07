29fabf983c7a652140ae430bc93a61e9e4cb7a9a
Matthew Sage <mattsage@users.noreply.github.com>
2017-08-07 15:01:18 +0100

    Update FileChecker.sh

--- FileChecker.sh
+++ FileChecker.sh
@@ -5,3 +5,14 @@
 # Date: 17/07/2017																																			          #                                                                    			   #
 # Description: Checks file location for finished downloads, once complete LED on GPIO ??? is lit  #
 ###################################################################################################
+#!/bin/bash
+foldercount=`ls /home/pi/Downloads/ -1 | wc -l`
+
+while [ $foldercount != 0 ]
+do
+	echo "$foldercount files left"
+	sleep 5m #Sleep for 5 Mins
+	foldercount=`ls /home/pi/Downloads/ -1 | wc -l`
+done
+
+echo "No Files left"
